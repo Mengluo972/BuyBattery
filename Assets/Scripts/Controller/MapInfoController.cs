35582ee8934e7e6d6c8a8f6d9ad4db21d743078f
Mengluo972 <147401756+Mengluo972@users.noreply.github.com>
2025-03-30 13:00:56 +0800

    将A星寻路更改为了曼哈顿寻路以适配障碍物

--- Assets/Scripts/Controller/MapInfoController.cs
+++ Assets/Scripts/Controller/MapInfoController.cs
@@ -4,24 +4,79 @@ using System.IO;
 using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.Serialization;
-
+/// <summary>
+/// 实现地图信息的读取和寻路管理
+/// </summary>
 public class MapInfoController : MonoBehaviour
 {
-    private List<List<AStarNode>> mapNodes = new List<List<AStarNode>>();
+    private static List<List<AStarNode>> mapNodes = new List<List<AStarNode>>();//不会随着脚本的销毁而销毁
+    private static List<List<Transform>> mapTransforms = new List<List<Transform>>();//不会随着脚本的销毁而销毁
     public string mapName;
-    public GameObject nodePrefab;
+    public GameObject nodePrefab;//预制体应当具有Node脚本
     public Transform originPosition;//应放在地图左下角格子处并且是格子的中心点(x.5,0,z.5)
-    private int _mapX;
-    private int _mapY;
+    private static int _mapX;
+    private static int _mapY;
     void Start()
     {
-        TextAsset xmlFile =  Resources.Load<TextAsset>($"/MapInfo/{mapName}.xml");
+        // LoadSceneData($"MapInfo/{mapName}");
+        LoadSceneData(6);
+    }
+
+    private static T ParseXML<T>(string xmlContent)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(T));
+        using StringReader reader = new StringReader(xmlContent);
+        return (T)serializer.Deserialize(reader);
+    }
+
+    // public void LoadSceneData(string dataName)
+    // {
+    //     TextAsset xmlFile =  Resources.Load<TextAsset>($"MapInfo/{mapName}");
+    //     if (xmlFile!=null)
+    //     {
+    //         mapNodes = ParseXML<List<List<AStarNode>>>(xmlFile.text);
+    //         // print("读取成功");
+    //     }
+    //     else return;
+    //
+    //     _mapX = mapNodes.Count-1;
+    //     _mapY = mapNodes[0].Count-1;
+    //
+    //     AStarManager.Instance.InitMapInfoWithPosition(_mapX,_mapY,mapNodes);
+    //     
+    //     for (int i = 0; i <= _mapX; i++)
+    //     {
+    //         for (int j = 0; j <= _mapY; j++)
+    //         {
+    //             Vector3 position = new Vector3(originPosition.position.x+i,0,originPosition.position.z+j);
+    //             GameObject nodeGameObject = Instantiate(nodePrefab,position,Quaternion.identity);
+    //             Node node = nodeGameObject.GetComponent<Node>();
+    //             node.AStarNode = mapNodes[i][j];
+    //         }
+    //     }
+    // }
+    
+    /// <summary>
+    /// 关卡文件名应为LevelX.xml，X为阿拉伯数字
+    /// </summary>
+    /// <param name="levelNum"></param>
+    public static void LoadSceneData(int levelNum)
+    {
+        TextAsset xmlFile = Resources.Load<TextAsset>($"MapInfo/Level{levelNum}");
         if (xmlFile!=null)
         {
             mapNodes = ParseXML<List<List<AStarNode>>>(xmlFile.text);
+            print("读取成功");
         }
         else return;
 
+        Transform originPos = GameObject.Find("OriginPosition").transform;//应放在地图左下角格子处并且是格子的中心点(x.5,0,z.5)，名字为OriginPosition
+        GameObject prefab = Resources.Load<GameObject>("MapInfo/NodePrefab/NodePrefab");//预制体放在Resources/MapInfo/NodePrefab文件夹下，名字为NodePrefab
+        
+        
+        // int mapX = mapNodes.Count-1;
+        // int mapY = mapNodes[0].Count-1;
+        
         _mapX = mapNodes.Count-1;
         _mapY = mapNodes[0].Count-1;
 
@@ -31,23 +86,25 @@ public class MapInfoController : MonoBehaviour
         {
             for (int j = 0; j <= _mapY; j++)
             {
-                Vector3 position = new Vector3(originPosition.position.x+i,0,originPosition.position.z+j);
-                GameObject nodeGameObject = Instantiate(nodePrefab,position,Quaternion.identity);
+                Vector3 position = new Vector3(originPos.position.x+i,0,originPos.position.z+j);
+                GameObject nodeGameObject = Instantiate(prefab,position,Quaternion.identity);
                 Node node = nodeGameObject.GetComponent<Node>();
                 node.AStarNode = mapNodes[i][j];
             }
         }
-        
-        
     }
-
-    private T ParseXML<T>(string xmlContent)
+    
+    public static List<Transform> AStarNodeToTransforms(List<AStarNode> path)
     {
-        XmlSerializer serializer = new XmlSerializer(typeof(T));
-        using StringReader reader = new StringReader(xmlContent);
-        return (T)serializer.Deserialize(reader);
-    }
+        List<Transform> transforms = new List<Transform>();
+        foreach (var node in path)
+        {
+            transforms.Add(mapTransforms[node.x][node.y]);
+        }
 
+        return transforms;
+    }
+    
     void Update()
     {
         
