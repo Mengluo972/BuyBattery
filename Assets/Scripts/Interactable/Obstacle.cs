0b0357b1cdaf7e24a77c5b6487e195b182c8da28
Mengluo972 <147401756+Mengluo972@users.noreply.github.com>
2025-04-27 18:06:58 +0800

    完成了动态障碍物的逻辑，未测试

---
+++ Assets/Scripts/Interactable/Obstacle.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour,IInteractable
+{
+    [Header("物品初始位置")]public Transform originPosition;
+    [Header("物品移动后位置")] public Transform targetPosition;
+    [Header("物品的移动时间")]public float time;
+    private bool _isMoving = false;
+    private int _place = 0;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TriggerAction()
+    {
+        if (!_isMoving)
+        {
+            StartCoroutine(MoveToTarget());
+        }
+    }
+    
+    private IEnumerator MoveToTarget()
+    {
+        _isMoving = true;
+        if (_place==0)
+        {
+            _place = 1;
+            transform.DOMove(targetPosition.position, time);
+            yield return new WaitForSeconds(time);
+        }
+        else
+        {
+            _place = 0;
+            transform.DOMove(originPosition.position, time);
+            yield return new WaitForSeconds(time);
+        }
+        _isMoving = false;
+    }
+
+    public void inTriggerAnimation(bool b)
+    {
+        
+    }
+}
