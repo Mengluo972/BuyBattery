5ef597c7e172185447aae962ea6df95b7bcd3399
Bedrydam <1074857534@qq.com>
2025-05-04 23:31:59 +0800

    1

--- Assets/Scripts/UI/SaveDataUI.cs
+++ Assets/Scripts/UI/SaveDataUI.cs
@@ -9,6 +9,8 @@ public class SaveDataUI : MonoBehaviour
     public GameObject parentObject;
 
     public float waitTime;
+    private PropManager propManager;
+
 
     // Start is called before the first frame update
     void Start()
