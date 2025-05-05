107a482a054daa22f15796d3d966863613a77933
Mengluo972 <147401756+Mengluo972@users.noreply.github.com>
2025-05-05 23:36:46 +0800

    完成了音频管理器的实现，具体的音效播放点未完成

--- Assets/Scripts/Controller/SoundManager.cs
+++ Assets/Scripts/Controller/SoundManager.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SoundType
+{
+    Fail,
+    FU,
+    Main,
+    Stealth,
+    Success
+}
 public class SoundManager
 {
-
+    private static readonly string soundPath = "Sound/";
+    private static readonly string bgmName = "BGM_";
     private static SoundManager _instance;
+    private static Dictionary<SoundType, AudioClip> _clips;
+    private AudioSource _audio;
     public static SoundManager Instance
     {
         get
@@ -13,10 +24,32 @@ public class SoundManager
             if (_instance == null)
             {
                 _instance = new SoundManager();
+                _clips = new Dictionary<SoundType, AudioClip>();
+                _clips.Add(SoundType.Fail, Resources.Load<AudioClip>(soundPath + "Fail"));
+                _clips.Add(SoundType.Success, Resources.Load<AudioClip>(soundPath + "Success"));
+                _clips.Add(SoundType.FU, Resources.Load<AudioClip>(soundPath + "FU"));
+                _clips.Add(SoundType.Main, Resources.Load<AudioClip>(soundPath + "Main"));
+                _clips.Add(SoundType.Stealth, Resources.Load<AudioClip>(soundPath + "Stealth"));
             }
             return _instance;
         }
     }
     //防止外部调用构造
     private SoundManager(){}
+
+    public void Play(SoundType type)
+    {
+        if (!_clips.TryGetValue(type,out AudioClip clip))
+        {
+            Debug.Log($"{type}音效播放失败");
+            return;
+        }
+        _audio.clip = clip;
+        _audio.Play();
+    }
+
+    public void SetLoop(bool isLoop)
+    {
+        _audio.loop = isLoop;
+    }
 }
