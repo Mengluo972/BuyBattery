b6b4be0e849b213b677ca858081033fd683b4ecf
Bedrydam <1074857534@qq.com>
2025-05-09 02:42:41 +0800

    1

---
+++ Assets/Scripts/Interactable/PrinterNoise.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrinterNoise : MonoBehaviour,IDoorControl
+{
+    public void DoorClose()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DoorOpen()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
